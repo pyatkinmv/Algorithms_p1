cc2ede36ae05bf526ad80d82f7e2a3637482ad7a
pyatkinmv <pyatkinmv@gmail.com>
2018-10-05 06:51:53 +0300

    Project frame is created

---
+++ PatternRecognition/src/BruteCollinearPoints.java
@@ -0,0 +1,26 @@
+// Write a program BruteCollinearPoints.java that examines 4 points at
+// a time and checks whether they all lie on the same line segment, returning all such line segments.
+// To check whether the 4 points p, q, r, and s are collinear,
+// check whether the three slopes between p and q, between p and r, and between p and s are all equal.
+// The method segments() should include each line segment containing 4 points exactly once.
+// If 4 points appear on a line segment in the order p→q→r→s, then you should include
+// either the line segment p→s or s→p (but not both) and you should not include subsegments
+// such as p→r or q→r. For simplicity, we will not supply any input to BruteCollinearPoints that has 5 or more collinear points.
+
+
+public class BruteCollinearPoints {
+    // finds all line segments containing 4 points
+    public BruteCollinearPoints(Point[] points) {
+
+    }
+
+//    // the number of line segments
+//    public int numberOfSegments() {
+//
+//    }
+//
+//    // the line segments
+//    public LineSegment[] segments() {
+//
+//    }
+}
---
+++ PatternRecognition/src/FastCollinearPoints.java
@@ -0,0 +1,18 @@
+//The method segments() should include each maximal line segment containing 4 (or more) points exactly once.
+// For example, if 5 points appear on a line segment in the order p→q→r→s→t,
+// then do not include the subsegments p→s or q→t.
+
+
+public class FastCollinearPoints {
+
+    // finds all line segments containing 4 or more points
+    public FastCollinearPoints(Point[] points) {
+
+    }
+
+//    // the number of line segments
+//    public int numberOfSegments()
+//
+//    // the line segments
+//    public LineSegment[] segments()
+}
